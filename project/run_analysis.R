b2d31a1291456ca4c3bd198d6122fcb88c136343
koeties <koeties@users.noreply.github.com>
2015-07-15 11:49:40 +0200

    Create run_analysis.R

---
+++ project/run_analysis.R
@@ -0,0 +1,60 @@
+# Read in our train and test data from the text files
+trainData <- read.table("./data/train/X_train.txt")
+trainLabel <- read.table("./data/train/y_train.txt")
+trainSubject <- read.table("./data/train/subject_train.txt")
+testData <- read.table("./data/test/X_test.txt")
+testLabel <- read.table("./data/test/y_test.txt")
+testSubject <- read.table("./data/test/subject_test.txt")
+
+# Combine the train and test data
+completeData <- rbind(trainData, testData)
+completeLabel <- rbind(trainLabel, testLabel)
+completeSubject <- rbind(trainSubject, testSubject)
+
+# Read in the labels of all the measurements from the text file
+features <- read.table("./data/features.txt")
+
+# Extract all the mean and standard deviation measure labels and drop the rest of
+# the measures from our data.
+meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+completeData <- completeData[, meanStdIndices]
+
+# Clean up column names by removing "(", ")" and "-" in column names
+names(completeData) <- gsub("\\(\\)", "", features[meanStdIndices, 2])
+names(completeData) <- gsub("-", "", names(completeData))
+
+# Read in our activity names from the text file, remove "_" characters and apply CamelCase.
+# Then replace the integer values in our data with the names
+activity <- read.table("./data/activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
+activityLabel <- activity[completeLabel[, 1], 2]
+completeLabel[, 1] <- activityLabel
+names(completeLabel) <- "activity"
+names(completeSubject) <- "subject"
+
+# Combine the Subject, Label and Data data frames and write it to a text file
+completeCleanData <- cbind(completeSubject, completeLabel, completeData)
+write.table(completeCleanData, "clean_merged_data.txt", row.name=FALSE)
+
+# Calculate the average of each variable for each activity and each subject.
+# Write the results to a text file.
+subjectLen <- length(table(completeSubject))
+activityLen <- dim(activity)[1]
+columnLen <- dim(completeCleanData)[2]
+result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen)
+result <- as.data.frame(result)
+colnames(result) <- colnames(completeCleanData)
+row <- 1
+for(i in 1:subjectLen) {
+    for(j in 1:activityLen) {
+        result[row, 1] <- sort(unique(completeSubject)[, 1])[i]
+        result[row, 2] <- activity[j, 2]
+        bool1 <- i == completeCleanData$subject
+        bool2 <- activity[j, 2] == completeCleanData$activity
+        result[row, 3:columnLen] <- colMeans(completeCleanData[bool1&bool2, 3:columnLen])
+        row <- row + 1
+    }
+}
+write.table(result, "clean_merged_data_with_means.txt", row.name=FALSE)
